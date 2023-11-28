e3a0f8035c7ce5d8f112dd7b31e32ae4b4234056
fabianame <nein.kkk@mail.com>
2023-11-28 18:29:06 +0100

    28.11

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ export function exercise01(args) {
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
+    if (currentElement != "e" currentElement !="E")
     result.push(currentElement)
   }
   return result.join("")
