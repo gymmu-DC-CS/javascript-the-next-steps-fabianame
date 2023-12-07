7aa4e435eac217595606c3be432246aa6e8f20ed
fabianame <nein.kkk@mail.com>
2023-12-07 15:43:22 +0100

    Ex 4, 7.12

--- script.js
+++ script.js
@@ -4,8 +4,30 @@ export function exercise01(args) {
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-    if (currentElement != "e" currentElement !="E")
-    result.push(currentElement)
+    console.log()
+    if (currentElement != "e" && currentElement != "E")
+      result.push(currentElement)
   }
+  console.log(result.join(""))
   return result.join("")
+}
+
+export function exercise02(args) {
+  return args.toUpperCase()
+}
+
+export function exercise03(args) {
+  const input = args
+  let count = 0
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+
+    if (currentElement === "e") {
+      count = count + 1
+    } else if (currentElement === "E") {
+      count = count + 1
+    }
+  }
+  return count
 }
